8ddfd1f4c2b254d80cb78556b99e0d3008674ca8
Anete Ots <anete.ots@tktk.ee>
2024-04-05 23:06:37 +0300

    add funtions code

---
+++ harjutus/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("How many steps have you walked today?");
+int usersSteps = Int32.Parse(Console.ReadLine());
+double result = HealthMonitor(usersSteps);
+Console.WriteLine();
+static double HealthMonitor(int usersSteps)
+{
+    if (usersSteps < 4000)
+    {
+        return ("You need to move more.");
+    }
+    else if (usersSteps >= 4000 && usersSteps < 8000)
+    {
+        return ("Health goal achieved,");
+    }
+    else
+    {
+        return ("Well done!");
+    }
+}
